77750389b419761e0d2541797f1fa30dfec220cf
John Goerzen <jgoerzen@complete.org>
2003-10-11 14:23:34 -0500

    Cleaned up files in debian/
    (jgoerzen@complete.org--projects/tla-buildpackage--head--1.0--patch-7)
    
    darcs-hash:20031011192334-c2a52-af9ba442f1d63a4e8a54d53f7264ec30797c0d2b.gz

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the tla-buildpackage package
-#
-0 4	* * *	root	tla-buildpackage_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(tla-buildpackage):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="tla-buildpackage" command="/usr/bin/tla-buildpackage"
