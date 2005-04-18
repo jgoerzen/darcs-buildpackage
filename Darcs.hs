4c888b258c2d98049522ad5e84f3570fc8adaacf
John Goerzen <jgoerzen@complete.org>
2005-04-18 16:43:59 -0500

    Added getTags (untested)
    darcs-hash:20050418214359-c2a52-e49ddc9d8db9283552150901cdef1a93d5e8bab7.gz

--- Darcs.hs
+++ Darcs.hs
@@ -8,6 +8,8 @@ module Darcs (bracketCWD)
 
 import System.Directory
 import Control.Exception
+import MissingH.Cmd
+import Text.Regex
 
 {- | Changes the current working directory to the given path,
 executes the given I\/O action, then changes back to the original directory,
@@ -17,3 +19,19 @@ bracketCWD fp action =
     do oldcwd <- getCurrentDirectory
        setCurrentDirectory fp
        finally action (setCurrentDirectory oldcwd)
+
+getTagsRe = mkRegex "^  tagged (.+)$"
+
+{- | Gets a list of all the tags in the given repository. -}
+getTags :: FilePath -> IO (PipeHandle, [String])
+getTags fp =
+    let procline line = 
+            case matchRegex getTagsRe line of
+              Nothing -> []
+              Just [x] -> [x]
+              Just _ -> error $ "Strange regexp result from " ++ line
+    in bracketCWD fp $
+         do (ph, lines) <- pipeLinesFrom "darcs" ["changes", "--patches=^TAG"]
+            return (ph, concatMap procline lines)
+
+         
