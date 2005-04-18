70ef34c338d739b498cf05ba7fdc9fcf85e3153e
John Goerzen <jgoerzen@complete.org>
2005-04-18 15:27:58 -0500

    Initial dbp-importorig complete
    darcs-hash:20050418202758-c2a52-82b8cacff71c53500d36b3b7449db1780c9dbd6a.gz

---
+++ dbp-importorig.hs
@@ -0,0 +1,29 @@
+{- Copyright (c) 2005 John Goerzen
+<jgoerzen@complete.org>
+Please see the COPYRIGHT file
+-}
+
+module Main where
+import Config
+import System.Environment
+
+usage = unlines $
+ ["Usage:",
+  "dbp-importorig orig-name package version",
+  "",
+  "Where:",
+  "  orig-name is the name of a tar.gz or directory to import",
+  "",
+  "package is the name of the Debian source package",
+  "",
+  "version is the upstream version being imported"]
+
+main = do args <- getArgs
+          (origname, package, version) = case args of
+           [x, y, z] -> return (x, y, z)
+           _ -> putStrLn usage
+                fail "Incorect command-line parameters."
+          isdir <- doesDirectoryExist origname
+          if isdir
+             then importOrigDir origname package version
+             else importOrigTarGz origname package version
