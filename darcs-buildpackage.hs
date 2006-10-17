2fb90a445f700e4fa3ff4f674c5dca601e673f11
John Goerzen <jgoerzen@complete.org>
2006-10-17 10:02:41 -0500

    UNDO: Checking in some odd code
    darcs-hash:20061017150241-c2a52-3b17f4a9d26a1b01ad9051cad40625b0d3a4ab1d.gz

--- darcs-buildpackage.hs
+++ darcs-buildpackage.hs
@@ -20,32 +20,8 @@ import System.Posix.Files
 import System.Cmd
 import System.Exit
 import Mirrors
-import List
-
-split :: Eq a => [a]
-      -> [a]
-      -> [[a]]
-
-split glue xs = split' xs
-    where
-    split' [] = []
-    split' xs = piece : split' (dropGlue rest)
-        where (piece, rest) = breakOnGlue glue xs
-    dropGlue = drop (length glue)
-
-breakOnGlue :: (Eq a) => [a]
-            -> [a]
-            -> ([a],[a])
-
-breakOnGlue _ [] = ([],[])
-breakOnGlue glue rest@(x:xs)
-    | glue `isPrefixOf` rest = ([], rest)
-    | otherwise = (x:piece, rest') where (piece, rest') = breakOnGlue glue xs
 
 main = do initLogging
-          args <- getArgs
-          let (dbp_args, rest) = partition (isPrefixOf "--dbp_builder=") (["--dbp_builder=debuild"] ++ args)
-          let build_cmd = last $ split "=" $ last dbp_args
           ver <- getVerFromCL
           pkg <- getPkgFromCL
           let (upsv, debv) = splitVer ver
@@ -56,7 +32,7 @@ main = do initLogging
           buildenv <- try (getEnv "DBP_BUILDER")
           case buildenv of
             Right buildcmd -> safeSystem "/bin/sh" ["-c", buildcmd]
-            Left _ -> safeSystem build_cmd (["-i_darcs", "-I_darcs"] ++ args)
+            Left _ -> safeSystem "debuild" (["-i_darcs", "-I_darcs"] ++ args)
 
 -- Build the orig.tar.gz
 buildorig pkg upsv debv =
